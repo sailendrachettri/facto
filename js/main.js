ee572e0bad149b1a3c74d3ba23d4830e5d4ef1f8
sailendrachettri <sailendra9083@gmail.com>
2022-12-14 10:35:45 +0530

    basic design is ready with working api/js

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c78fb667a3msh7d7e2fb02212684p19c0a3jsn6c1847f46e85',
+		'X-RapidAPI-Host': 'facts-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+
+
+function changeFact() {
+
+	fetch('https://facts-by-api-ninjas.p.rapidapi.com/v1/facts', options)
+		.then(response => response.json())
+		.then((response) => {
+			// console.log(response);
+
+			factInfo.innerHTML = response[0].fact;
+
+
+
+		})
+		.catch(err => console.error(err));
+
+}
+
+changeFact();
+
+setInterval(changeFact, 20000);
