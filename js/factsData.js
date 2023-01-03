bc365d51685101cac3fb237db7a71bdb9094714a
sailendrachettri <sailendra9083@gmail.com>
2023-01-03 16:50:05 +0530

    count down added

--- js/factsData.js
+++ js/factsData.js
@@ -6,29 +6,18 @@ const options = {
 	}
 };
 
+let factInfo = document.getElementById('factInfo');
+
 function changeFact() {
 
 	fetch('https://facts-by-api-ninjas.p.rapidapi.com/v1/facts', options)
 		.then(response => response.json())
 		.then((response) => {
-			// console.log(response);
-
 			factInfo.innerHTML = response[0].fact + '.';
-
-
-
 		})
 		.catch(err => console.error(err));
 
 }
-
 changeFact();
+setInterval(changeFact, 11000);
 
-setInterval(changeFact, 15000);
-
-
-// _________________________________FOOTER STARTED_________________________________
-let date = new Date();
-let year = date.getFullYear();
-footerYear.innerHTML = year;
-// _________________________________FOOTER ENDED_________________________________
